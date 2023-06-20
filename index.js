1f394724334031f20da5b8f748b6363fba453753
rekktt <136921157+rekktt@users.noreply.github.com>
2023-06-20 18:45:03 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+ }
+
+ function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+ }
+
+ function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+    return cats;
+ }
+
+ function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+    return cats;
+ }
+ 
+ function appendCat(name){
+    var name_1= [...cats,name];
+    return name_1;
+  }
+
+ 
+    function prependCat(name){
+        var newArray = [...cats];
+        newArray.unshift(name);
+        return newArray;
+    }
+
+    function removeLastCat() {
+        var newArray = [...cats];
+        newArray.pop();
+        return newArray;
+    }
+
+    function removeFirstCat() {
+        var newArray=[...cats];
+        newArray.shift();
+        return newArray;
+    }
